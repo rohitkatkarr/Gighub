3c6bf55db295e4ae44aa75d8b3c7ee96cf30c620
Rohit Katkar <katkarrohit59@gmail.com>
2025-04-02 13:18:15 -0400

    Final version

---
+++ client/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHHLk7DxUXCTdYxa1B957K-4eFncb74F4",
+  authDomain: "gighub-c6d1c.firebaseapp.com",
+  projectId: "gighub-c6d1c",
+  storageBucket: "gighub-c6d1c.firebasestorage.app",
+  messagingSenderId: "130797845481",
+  appId: "1:130797845481:web:6ae086e02c6d00a0402978"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,179 @@
+import React, { useEffect, useState } from "react";
+import Banner from "../components/Banner";
+import Sidebar from "../Sidebar/Sidebar";
+import Jobs from "./Jobs";
+import Card from "../components/Card";
+import Newsletter from "../components/Newsletter";
+
+const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [jobs, setJobs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:5000/all-jobs")
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  // ----------- Input Filter -----------
+  const [query, setQuery] = useState("");
+  const [location, setLocation] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "query") {
+      setQuery(value);
+    } else if (name === "location") { 
+      setLocation(value);
+    }
+  };
+
+  const filteredItems = jobs.filter(
+    (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+
+  // ----------- Radio Filtering -----------
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  // ------------ Button Filtering -----------
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  // Function to calculate the index range for the current page
+  const calculatePageRange = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return { startIndex, endIndex };
+  };
+
+  // Function to handle next page
+  const nextPage = () => {
+    if (currentPage < Math.ceil(filteredItems.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Function to handle previous page
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const filteredData = (jobs, selected, query, location) => {
+    let filteredJobs = jobs;
+    // Filtering Input Items
+
+    // Apply both job title and location filtering together
+    filteredJobs = filteredJobs.filter((job) => {
+      const matchesTitle = query
+        ? job.jobTitle.toLowerCase().includes(query.toLowerCase())
+        : true; // If no query, return all jobs
+
+      const matchesLocation = location
+        ? job.jobLocation.toLowerCase().includes(location.toLowerCase())
+        : true; // If no location, return all jobs
+
+      return matchesTitle && matchesLocation; // Ensure BOTH conditions are met
+    });
+
+    // Applying selected filter
+    if (selected) {
+      filteredJobs = filteredJobs.filter(
+        ({
+          jobLocation,
+          salaryType,
+          experienceLevel,
+          maxPrice,
+          postingDate,
+          employmentType,
+        }) =>
+          jobLocation.toLowerCase() === selected.toLowerCase() ||
+          postingDate === selected ||
+          parseInt(maxPrice) <= parseInt(selected) ||
+          salaryType.toLowerCase() === selected.toLowerCase() ||
+          experienceLevel.toLowerCase() === selected.toLowerCase() ||
+          employmentType.toLowerCase() === selected.toLowerCase()
+      );
+    }
+
+    // Slice the data based on the current page
+    const { startIndex, endIndex } = calculatePageRange();
+    filteredJobs = filteredJobs.slice(startIndex, endIndex);
+
+    return filteredJobs.map((data, i) => <Card key={i} data={data} />);
+  };
+
+  const result = filteredData(jobs, selectedCategory, query, location);
+
+  return (
+    <div>
+      <Banner query={query} handleInputChange={handleInputChange} location={location}/>
+
+      {/* main content */}
+      <div className="bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
+        <div className="bg-white p-4 rounded">
+          <Sidebar handleChange={handleChange} handleClick={handleClick} />
+        </div>
+        <div className="col-span-2 bg-white p-4 rounded">
+          {isLoading ? ( // Loading indicator
+            <p className="font-medium">Loading...</p>
+          ) : result.length > 0 ? (
+            <Jobs result={result} />
+          ) : (
+            <>
+              <h3 className="text-lg font-bold mb-2">{result.length} Jobs</h3>
+              <p>No data found</p>
+            </>
+          )}
+
+          {/* pagination block here */}
+
+          {result.length > 0 ? (
+            <div className="flex justify-center mt-4 space-x-8">
+              <button
+                onClick={prevPage}
+                disabled={currentPage === 1}
+                className="hover:underline"
+              >
+                Previous
+              </button>
+              <span className="mx-2">
+                Page {currentPage} of{" "}
+                {Math.ceil(filteredItems.length / itemsPerPage)}
+              </span>
+              <button
+                onClick={nextPage}
+                disabled={
+                  currentPage === Math.ceil(filteredItems.length / itemsPerPage)
+                }
+                className="hover:underline"
+              >
+                Next
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="bg-white p-4 rounded">
+          <Newsletter />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
